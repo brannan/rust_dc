61a175700a72f0f79d514988374d6d563370979b
P. S. Brannan <brannan@foxtraxgps.com>
2023-01-30 20:27:56 -0600

    monsters

---
+++ src/components.rs
@@ -0,0 +1,13 @@
+pub use crate::prelude::*;
+
+#[derive(Clone, Copy, PartialEq)]
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub struct Player;
+
+#[derive(Clone, Copy, PartialEq)]
+pub struct Enemy;
