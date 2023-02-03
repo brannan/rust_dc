666d5a23604e7eb6209e9f50497905f403f9f894
P. S. Brannan <brannan@foxtraxgps.com>
2023-02-02 21:51:42 -0600

    hud & tooltips

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,11 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
+        ctx.set_active_console(2);
+        ctx.cls();
         self.resources.insert(ctx.key);
+        ctx.set_active_console(0);
+        self.resources.insert(Point::from_tuple(ctx.mouse_pos()));
         let current_state = self.resources.get::<TurnState>().unwrap().clone();
         match current_state {
             TurnState::AwaitingInput => self
@@ -95,8 +99,10 @@ fn main() -> BError {
         .with_tile_dimensions(32, 32) // (3)
         .with_resource_path("resources/") // (4)
         .with_font("dungeonfont.png", 32, 32) // (5)
+        .with_font("terminal8x8.png", 8, 8) // (5)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (6)
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (7)
+        .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "terminal8x8.png") // (6)
         .build()?;
 
     main_loop(context, State::new())
